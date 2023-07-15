fc36d39aefe86ec1085219ccd3af32ead91eae11
Devendra Yadav <devendra.yadav@gmail.com>
2023-07-16 00:21:12 +0800

    initial commit

---
+++ src/main/java/com/hh/HealingHouseApplication.java
@@ -0,0 +1,13 @@
+package com.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealingHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hh/HealingHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
