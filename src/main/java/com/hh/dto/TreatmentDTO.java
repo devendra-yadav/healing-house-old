0e0f9d7dd12e99ffa341a2dc0e6ec5545169e002
Devendra Yadav <Devendra_Yadav@epam.com>
2023-08-08 13:09:34 +0800

    updated treatment dto

--- src/main/java/com/hh/dto/TreatmentDTO.java
+++ src/main/java/com/hh/dto/TreatmentDTO.java
@@ -25,17 +25,102 @@ public class TreatmentDTO {
 	private boolean fullyPaid;
 	
 	
-	//Initial Enquiry
+	//Enquiry : SPIRIT
 	private String complexion;
 	private String eyes;
 	private String stateOfMind;
 	
-	private String leftCheekLiver;
-	private String chinKidney;
-	private String nodeSpleen;
-	private String rightCheekLung;
 	
-	private String darkSpots;
+	//Enquiry : Appearance
+	private boolean movementAndTranquility;
+	private boolean coughAndPanting;
+	private boolean convulsiveSpasm;
+	private boolean paralysis;
+	private boolean chestAbdomenPattern;
+	private boolean delirious;
+	private boolean overweight;
+	private boolean skinny;
+	private boolean violentMovementsForLimbs;
+	private boolean face;
+	
+	//Enquiry : Listening Voice;
+	private boolean deliriousSpeak;
+	private boolean mumbling;
+	private boolean lossOfVoice;
+	
+	//Enquiry : Respiration
+	private boolean breathlessNess;
+	private boolean wheezing;
+	private boolean shortnessOfBreath;
+	private boolean panting;
+	
+	//Enquiry : Smelling
+	private String breathOdor;
+	private String sweatOdod;
+	private boolean odorUnderArmpit;
+	
+	//Enquiry : Food and appetite
+	private boolean poorAppetite;
+	private boolean lackOfAppetiteWithFullnessOfStomach;
+	private boolean hunderWithNoDesireToEat;
+	private boolean getHungryEasy;
+	private boolean inabilityToFeelTaste;
+	private String taste;
+	
+	//Enquiry : Chills and fever
+	private boolean patternOfExteriorInteriorOrBoth;
+	private boolean alternateChillsOrFever;
+	private boolean persistentHighFever;
+	private boolean middlefever;
+	private boolean tidalFever;
+	private boolean irritableVexingFever;
+	
+	//Enquiry : Stool and urine
+	private String stoolConsistency;
+	private boolean flatulency;
+	private boolean prolapseAnus;
+	private boolean hemorrhoids;
+	private String urine;
+	
+	//Enquiry : Sweating and perspiration
+	private boolean spontaneousSweating;
+	private boolean nightSweating;
+	private boolean yellowSweatingStainOnClothes;
+	private boolean expiringSweating;
+	
+	//Enquiry : insomnia
+	private boolean difficultToSleep;
+	private boolean sleepButKeepWakingUp;
+	private boolean fearToFallSleep;
+	private boolean sleepSomethingPullBack;
+	private boolean sleepButWakeupInMiddleNightSameTime;
+	private boolean tooManyThoughtsBeforeSleep;
+	private boolean tooMuchDreams;
+	private String dreamsAboutWhat;
+	
+	//Enquiry: Tinnitus
+	private boolean loudHighPitch;
+	private boolean lowPichNoise;
+	private boolean deafnessOrHearingImpairment;
+	private boolean redPainFullEyes;
+	private boolean blurryCloudyVision;
+	
+	//Enquiry : Menstruration
+	private boolean mensturalDisorder;
+	private boolean mensturalPain;
+	private boolean ysmenorrhea;
+	private boolean leucorrhea;
+	private boolean vaginalDischarge;
+	private boolean frequency;
+	private boolean mensBloodQuantity;
+	private boolean mensBloodColor;
+	private boolean mensBloodSmell;
+	
+	//Enquiry : others
+	private String pain;
+	private boolean dizziness;
+	
+	
 	
 	
 	
